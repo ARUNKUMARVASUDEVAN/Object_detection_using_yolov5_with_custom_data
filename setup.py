fd2f25b3be1eb6e266de5435af2edc9c0f54d091
ARUNKUMAR V <109424750+ARUNKUMARVASUDEVAN@users.noreply.github.com>
2023-11-16 11:55:29 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'American signLanguages',
+    version= '0.0.0',
+    author= 'Arunkumar',
+    author_email= 'oyeahitsarun@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
